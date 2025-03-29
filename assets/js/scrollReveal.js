88c10fa809742272a4f478188bf9e0e36d9e486f
Mayke Willians <maykewillians3@gmail.com>
2025-03-29 14:20:40 -0300

    fix: menu

--- assets/js/scrollReveal.js
+++ assets/js/scrollReveal.js
@@ -1,13 +1,13 @@
 export function initScrollReveal() {
   const sr = ScrollReveal({
     origin: "top",
-    distance: "50px",
-    duration: 2000,
+    distance: "10px",
+    duration: 1000,
   });
 
   sr.reveal(".delaySmallReveal", { delay: 200 });
-  sr.reveal(".delayMediumReveal", { delay: 300 });
-  sr.reveal(".delayLargeReveal", { delay: 400 });
-  sr.reveal(".delayExtraBigReveal", { delay: 600 });
-  sr.reveal(".intervalCardReveal", { interval: 400 });
+  sr.reveal(".delayMediumReveal", { delay: 200 });
+  sr.reveal(".delayLargeReveal", { delay: 200 });
+  sr.reveal(".delayExtraBigReveal", { delay: 200 });
+  sr.reveal(".intervalCardReveal", { interval: 200 });
 }
